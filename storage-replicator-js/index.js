00d33af60dcffa35a913d3f761162960c6f45bf1
peteroden <pete.roden@live.com>
2017-08-14 09:02:45 -0400

    code cleaned up and ready

--- storage-replicator-js/index.js
+++ storage-replicator-js/index.js
@@ -1,5 +1,5 @@
-module.exports = function (context, inputBlob, outputBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
-    outputBlob = inputBlob;
+module.exports = function (context) {
+    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", context.bindings.inputBlob.length, "Bytes");
+    context.bindings.outputBlob = context.bindings.inputBlob;
     context.done();
 };
